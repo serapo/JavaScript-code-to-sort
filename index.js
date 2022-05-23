65d3b2fedddb749679004658e5e304ce74c3026e
Serap <77828715+serapo@users.noreply.github.com>
2022-05-23 13:29:49 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+console.log("******************");
+list = [999, 333, 2, 8982, 12, 45, 77, 99, 11]
+console.log("Mevcut liste:"+list);
+console.log("******************");
+list.sort(function(a,b){return a-b});
+
+console.log("Sıralı liste :"+list);
